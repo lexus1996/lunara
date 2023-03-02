6475ebbe12efe4b21b78ad40f4558863c38c6a17
Alex <alex.pirvulescu96@gmail.com>
2023-03-03 00:19:00 +0200

    First push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'lunara';
+  count: number = 0;
+  countUp() {
+    this.count++;
+  } 
 }
+
