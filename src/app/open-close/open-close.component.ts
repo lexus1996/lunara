20abb4a13ffdc48d2e7b269f8cf91e6414a3bb01
Alex <alex.pirvulescu96@gmail.com>
2023-03-10 22:44:45 +0200

    Test animation

--- src/app/open-close/open-close.component.ts
+++ src/app/open-close/open-close.component.ts
@@ -1,9 +1,32 @@
+import { trigger, state, style, transition, animate } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-open-close',
   templateUrl: './open-close.component.html',
   styleUrls: ['./open-close.component.scss'],
+  animations: [
+    trigger('openClose', [
+      // ...
+      state('open', style({
+        height: '200px',
+        opacity: 1,
+        backgroundColor: 'yellow'
+      })),
+      state('closed', style({
+        height: '70rem',
+        opacity: 0.8,
+        backgroundColor: 'blue'
+      })),
+      transition('open => closed', [
+        animate('1s')
+      ]),
+      transition('closed => open', [
+        animate('0.5s')
+      ]),
+    ]),
+  ],
 })
 
 export class OpenCloseComponent implements OnInit {
@@ -11,7 +34,11 @@ export class OpenCloseComponent implements OnInit {
 
   constructor() { }
 
+  isOpen = true;
 
+  toggle() {
+    this.isOpen = !this.isOpen;
+  }
 
 
 
